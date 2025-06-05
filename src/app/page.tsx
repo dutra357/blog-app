1b0073bbcea15e3fcd0483acc8112749835f59ad
David Alves Dutra <dadutra@hotmail.com>
2025-06-05 20:02:06 -0300

    POST: Criada orientação do post principal. Ajuste css com tailwind. Orientação de texto e protótipo de componente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,54 @@
 import { Container } from "@/components/container";
-import { PostList } from "@/components/post-list/PostList";
-import { SpinLoader } from "@/components/spin-loader/SpinLoader";
+import { Header } from "@/components/header";
+import { PostList } from "@/components/post-list";
+import { SpinLoader } from "@/components/spin-loader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home() {
 
   return (
     <Container>
+      <Header />
 
-      <header>
-        <h1 className='text-6x1 font-bold text-center py-8'>Blog App</h1>
-      </header>
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
 
-      <p className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Ipsa quisquam ut ipsam asperiores sit rerum dolor possimus vero,
-        dignissimos, et fugiat ex maiores officia, dolores consequuntur
-        architecto repudiandae. Corporis, blanditiis.
-      </p>
+        <Link href="#" className='w-full h-full overflow-hidden rounded-xl'>
+          
+          <Image className='object-cover object-center w-full h-full group-hover:scale-105 transition'
+            priority
+            src='/images/image.png'
+            width={1200}
+            height={720}
+            alt='Título do post' />
+
+        </Link>
+
+        <div className='flex flex-col gap-2'>
+          <time className='text-slate-600 text-sm/tight' dateTime='2025-04-21'>21 de abril de 2025</time>
+
+          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
+            <Link href='#'>Meu link</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Laudantium unde quis itaque quos asperiores, libero aspernatur
+            tenetur fugiat voluptate ipsa nemo earum illo
+            consequuntur enim architecto officiis, esse tempore repellat.
+          </p>
+        </div>
+
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
 
       <footer>
-        <h2 className='text-6x1 font-bold text-center py-8'>Meu footer</h2>
+        <h1 className='text-6x1 font-bold text-center py-8'>Meu footer</h1>
       </footer>
 
     </Container>
