c682c792de0002280ff1519e1da8f8fc7c93e2d8
David Alves Dutra <dadutra@hotmail.com>
2025-06-09 08:22:34 -0300

    META: Ajustes para metadata, com título. Ajustado layout geral, e segregados os componentes da home.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'About'
+}
+
+export default function Aboutpage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a simple about page.</p>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,19 @@
-import { Container } from "@/components/container";
 import { PostDestaque } from "@/components/destaque";
-import { Header } from "@/components/header";
 import { PostList } from "@/components/post-list";
 import { SpinLoader } from "@/components/spin-loader";
 import { Suspense } from "react";
 
+
 export default async function Home() {
-
   return (
-    <Container>
-      <Header />
 
+    <>
       <PostDestaque />
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
+    </>
 
-      <footer>
-        <h1 className='text-6x1 font-bold text-center py-8'>Meu footer</h1>
-      </footer>
-
-    </Container>
   );
 }
