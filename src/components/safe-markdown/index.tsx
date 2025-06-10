e45d7bd82698aed37ef410e60195b22fd508eaa7
David Alves Dutra <dadutra@hotmail.com>
2025-06-10 08:43:20 -0300

    TAILWIND: Adicionado plugin para estilização. tailwind/typography.

--- src/components/safe-markdown/index.tsx
+++ src/components/safe-markdown/index.tsx
@@ -9,10 +9,29 @@ type SafeMarkDown = {
 
 export function SafeMarkDown({ markdown }: SafeMarkDown) {
     return (
-        <ReactMarkdown 
-            rehypePlugins={[rehypeSanitize]} 
-            remarkPlugins={[remarkGfm]}>
-            {markdown}
-        </ReactMarkdown>
+        <div className='prose prose-slate w-full max-w-none
+         overflow-hidden prose-a:text-blue-500 prose-a:hover:text-blue-800
+          prose-a:transition prose-a:no-underline prose-a:hover:underline 
+          prose-img:mx-auto md:prose-lg'>
+
+            <ReactMarkdown
+                rehypePlugins={[rehypeSanitize]}
+                remarkPlugins={[remarkGfm]}
+                components={{
+                    table: ({ node, ...props }) => {
+                        if (!node?.children) {
+                            return '';
+                        }
+
+                        return (
+                        <div className='overflow-x-auto'>
+                            <table className='w-full min-w-[600]' {...props} />
+                        </div>
+                    )},
+                }} >
+                {markdown}
+            </ReactMarkdown>
+        </div>
+
     );
 }
